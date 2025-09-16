7501f70ea1f7a4abeefb7c01a90b800d84a9ca57
JENNYKIM74 <h2844j@gmail.com>
2025-09-16 18:20:49 +0900

    chore: 멀티모듈 설정

---
+++ coupon-consumer/src/test/java/kr/co/couponconsumer/CouponConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.couponconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
